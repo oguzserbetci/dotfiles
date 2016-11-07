b31476d5f8f68899101c296051d0948c73e8a4e8
Oguz Serbetci <oguz.serbetci@gmail.com>
2016-11-07 20:14:34 +0100

    work

--- install.sh
+++ install.sh
@@ -7,17 +7,35 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Bunch of symlinks
 ln -sfv "$DOTFILES_DIR/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/.kwm" ~
+ln -sfv "$DOTFILES_DIR/.khdrc" ~
 ln -sfv "$DOTFILES_DIR/.functions" ~
-ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/.gitignore" ~
+
 
 # Package managers & packages
 
 echo "› brew bundle"
-# brew bundle
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+brew bundle
+
+brew services start koekeishiya/khd/khd
+brew services start koekeishiya/kwm/kwm
+
+chsh -s /usr/local/bin/zsh-5.2
+
+rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+ln -sfv ~/Dropbox/Sublime/Packages ~/Library/Application\ Support/Sublime\ Text\ 3/
 
 . "$DOTFILES_DIR/osxdefaults.sh"
 
-git clone git://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_basic_vimrc.sh
+git clone git://github.com/amix/vimrc.git ~/.dotfiles/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+ln -sfv "$DOTFILES_DIR/.vim_runtime" ~
